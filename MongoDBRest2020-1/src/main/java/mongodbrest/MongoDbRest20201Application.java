65cb340fe77e5b8b48a8f621512a3e0bc24bd22f
glaydson <glaydsonvs@gmail.com>
2020-03-06 17:31:55 -0300

    Commit inicial

---
+++ MongoDBRest2020-1/src/main/java/mongodbrest/MongoDbRest20201Application.java
@@ -0,0 +1,13 @@
+package mongodbrest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoDbRest20201Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoDbRest20201Application.class, args);
+	}
+
+}
---
+++ MongoDBRest2020-1/src/test/java/mongodbrest/MongoDbRest20201ApplicationTests.java
@@ -0,0 +1,13 @@
+package mongodbrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoDbRest20201ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
